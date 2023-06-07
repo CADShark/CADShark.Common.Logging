936db6b51d93f9ce0c2c4da32fdb4624c3dd18fe
Denis <4uzhoy@gmail.com>
2023-06-07 20:32:41 +0300

    add packages

--- Logger.cs
+++ Logger.cs
@@ -4,40 +4,31 @@ namespace CADShark.Common.Logging
 {
     public class Logger
     {
-        //private static NLog.Logger _logger;
         private static readonly ILogger Loggers = LogManager.GetCurrentClassLogger();
-
         public Logger()
         {
-
             LoggingConfiguration.Configure();
         }
-
         public static void Debug(string message)
         {
             Loggers.Debug(message);
         }
-
         public void Info(string message)
         {
             Loggers.Info(message);
         }
-
         public void Warning(string message)
         {
             Loggers.Warn(message);
         }
-
         public void Error(string message)
         {
             Loggers.Error(message);
         }
-
         public void Fatal(string message)
         {
             Loggers.Fatal(message);
         }
-
         public void Trace(string message)
         {
             Loggers.Trace(message);
